221bcdba754a969527e7d48c18e490edd88a848a
larmong <larmong@naver.com>
2023-09-10 23:42:38 +0900

    class 전략 패턴

---
+++ section06/06-03-class-and-OOP-strategy/index.js
@@ -0,0 +1,38 @@
+class 공중부품 {
+  run = () => {
+    console.log("날라서 도망가자!!!")
+  }
+}
+
+class 지상부품 {
+  run = () => {
+    console.log("뛰어서 도망가자!!!")
+  }
+}
+
+class Monster {
+  power = 10
+  부품;
+
+  // 생성자
+  constructor(qqq) {
+    this.부품 = qqq
+  }
+
+  attack = () => {
+    console.log("공격하자!!!")
+    console.log(`내 공격력은 ${this.power}이야!!!`)
+  }
+
+  run = () => {
+    this.부품.run()
+  }
+}
+
+const myMonster1 = new Monster(new 공중부품())
+myMonster1.attack()
+myMonster1.run()
+
+const myMonster2 = new Monster(new 지상부품())
+myMonster2.attack()
+myMonster2.run()
