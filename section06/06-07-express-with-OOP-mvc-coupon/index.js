49a96a45c905fbdd29ba40cd7e2383753b989e99
larmong <larmong@naver.com>
2023-09-17 22:34:27 +0900

    MVC Pattern 실습

--- section06/06-07-express-with-OOP-mvc-coupon/index.js
+++ section06/06-07-express-with-OOP-mvc-coupon/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { ProductController } from "./mvc/controllers/product.contriller.js";
+import { CouponController } from "./mvc/controllers/coupon.controller.js";
 
 const app = express()
 
@@ -8,6 +9,10 @@ const app = express()
 const productController = new ProductController();
 app.post('/products/buy', productController.buyProduct)  // 상품 구매하기 API
 app.post("/products/refund",  productController.refund)  // 상품 환불하기 API
+
+// 쿠폰(상품권) API
+const couponController = new CouponController();
+app.post("/coupons/buy", couponController.buyCoupon)  // 쿠폰(상품권) 구매하기 API
 
 // 게시판 API
 // app.get("")
