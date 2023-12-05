85b86795d4fdf8657097652a011fe076cf4523d8
larmong <larmong@naver.com>
2023-12-05 15:24:17 +0900

    이메일 템플릿 생성2 - shorthand property

---
+++ section01/01-05-shorthand-property/index.js
@@ -0,0 +1,11 @@
+function qqq(aaa){
+  console.log(aaa)
+}
+
+const name = "철수"
+const age = 12
+const school = "다람쥐초등학교"
+
+const profile = { name, age, school }  // key와 value가 같아서 value를 생략함 => shorthand property
+
+qqq(profile)
