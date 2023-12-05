61981145db0debdb3908c4fb06c64c069cd669e7
larmong <larmong@naver.com>
2023-12-05 15:13:05 +0900

    이메일 템플릿 생성1

---
+++ section01/01-04-welcom-template/index.js
@@ -0,0 +1,26 @@
+function getWelcomeTemplate({name, age, school, createdAt}){
+  const template = `
+    <html>
+      <body>
+        <h1>${name}님 가입을 환영합니다!!!</h1>
+        <hr />
+        <div>이름: ${name}</div>
+        <div>나이: ${age}살</div>
+        <div>학교: ${school}</div>
+        <div>가입일: ${createdAt}</div>
+      </body> 
+    </html>
+  `
+
+  console.log(template)
+}
+
+
+const data = {
+  name: "철수",
+  age: 12,
+  school: "다람쥐초등학교",
+  createdAt: "2023-12-05"
+}
+
+getWelcomeTemplate(data)
