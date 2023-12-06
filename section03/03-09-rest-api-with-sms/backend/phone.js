b70261a3728da97cb72770d8f182d5e500170e50
larmong <larmong@naver.com>
2023-12-06 16:44:08 +0900

    핸드폰으로 인증번호 보내기(coolsms)

---
+++ section03/03-09-rest-api-with-sms/backend/phone.js
@@ -0,0 +1,29 @@
+import coolsms from "coolsms-node-sdk"
+const smsService = coolsms.default
+
+export function checkPhone(phone){
+  if(phone.length < 10 || phone.length > 11){
+    console.log("핸드폰번호를 제대로 입력해 주세요!!");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+export function getToken(){
+  const result = String(Math.floor(Math.random() * 1000000)).padStart(6, "0")
+  return result;
+}
+
+export async function sendTokenToSMS(phone, token){
+  // console.log(`${phone} 번호로 인증번호 ${token}을 전송했습니다!!`)
+
+  const messageService = new smsService('', '');
+  const result = await messageService.sendOne({
+    to: phone,
+    from: '01092222240',
+    text: `[테스트] 요청하신 인증번호는 ${token} 입니다.`
+  })
+  console.log(result)
+
+}
