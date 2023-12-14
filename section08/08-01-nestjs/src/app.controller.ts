2488f4280163242c58c318c0c4ee6ddd0c163156
larmong <larmong@naver.com>
2023-12-14 17:49:09 +0900

    nestjs 기본 구조 + graphql

--- section08/08-01-nestjs/src/app.controller.ts
+++ section08/08-01-nestjs/src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/') // 엔드포인트
   getHello(): string {
     const qqq = 3;
     console.log(qqq);
