8f10a122604d9df1bdf726a88e790fb9b71f0f28
larmong <larmong@naver.com>
2024-05-09 15:24:35 +0900

    01-07-welcome-template-api

---
+++ section01/01-07-welcome-template-api/index.js
@@ -0,0 +1,53 @@
+function checkEmail(email){
+  if(!email || !email.includes("@")){
+    console.log("error! 이메일을 확인해주세요!");
+    return false;
+  }
+  return true;
+}
+
+function getWelcomeTemplate({ name, age, school, createdAt }){
+  return `
+    <html>
+      <body>
+        <h1>${name}님 가입을 환영합니다!!!</h1>
+        <hr />
+        <div>이름: ${name}</div>
+        <div>나이: ${age}살</div>
+        <div>학교: ${school}</div>
+        <div>가입일: ${createdAt}</div>
+      </body>
+    </html>
+  `
+}
+
+function sendTokenToEmail({ email, template }){
+  console.log(`
+    "${email}"이메일로 
+    ${template}
+    가입환영 템플릿을 전송했습니다!
+  `);
+}
+
+
+function createUser({ name, age, school, email, createdAt }){
+  // 1. 이메일이 정상인지 확인(1. 존재여부, 2. "@" 포함 여부)
+  const isEmail = checkEmail(email);
+  if(!isEmail) return;
+
+  // 2. 가입 환영 템플릿 만들기
+  const template = getWelcomeTemplate({ name, age, school, createdAt });
+
+  // 3. 이메일에 가입환영 템플릿 전송하기
+  sendTokenToEmail({ email, template })
+}
+
+const newUser = {
+  name: "철수",
+  age: "8",
+  school: "다람쥐초등학교",
+  email: "aa.com",
+  createdAt: "2024-05-09",
+}
+
+createUser(newUser);
