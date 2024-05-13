de51d659fbbb433befef371942567e8d2d0d92a1
larmong <larmong@naver.com>
2024-05-13 15:21:53 +0900

    07-03-typescript-private-readonly

---
+++ section07/07-03-typescript-private-readonly/01-public.ts
@@ -0,0 +1,33 @@
+export {}
+// public
+// 원본 Monster        : 읽기 o, 쓰기 o
+// 자식 ChangeMonster  : 읽기 o, 쓰기 o
+// 함수 밖              : 읽기 o, 쓰기 o
+
+// class Monster {
+//     // power = 10;                       ===>   public, private, protected, readonly 중에 1개라도 있으면 생략 가능!
+//
+//     constructor(public power: any) {
+//         // this.power = power;           ===>   public, private, protected, readonly 중에 1개라도 있으면 생략 가능!
+//     }
+//
+//     attack1(){
+//         console.log("공격하자!!!")
+//         console.log(`내 공격력은 ${this.power}야!!!`)
+//         this.power = 30;
+//     }
+// }
+//
+// class ChangeMonster extends Monster {
+//     attack2(){
+//         console.log("공격하자!!!")
+//         console.log(`내 공격력은 ${this.power}야!!!`)
+//     }
+// }
+//
+// const myMonster = new ChangeMonster(20);
+// myMonster.attack1();
+// myMonster.attack2();
+// console.log(myMonster.power);
+//
+// myMonster.power = 10;
---
+++ section07/07-03-typescript-private-readonly/05-private-readonly.ts
@@ -0,0 +1,34 @@
+export {}
+// private readonly
+// 원본 Monster        : 읽기 o, 쓰기 x
+// 자식 ChangeMonster  : 읽기 x, 쓰기 x
+// 함수 밖              : 읽기 x, 쓰기 x
+
+class Monster {
+    // power = 10;                       ===>   public, private, protected, readonly 중에 1개라도 있으면 생략 가능!
+
+    constructor(private readonly power: any) {
+        // this.power = power;           ===>   public, private, protected, readonly 중에 1개라도 있으면 생략 가능!
+    }
+
+    attack1(){
+        console.log("공격하자!!!")
+        console.log(`내 공격력은 ${this.power}야!!!`)
+        this.power = 30;
+    }
+}
+
+class ChangeMonster extends Monster {
+    attack2(){
+        console.log("공격하자!!!")
+        console.log(`내 공격력은 ${this.power}야!!!`)
+        this.power = 30;
+    }
+}
+
+const myMonster = new ChangeMonster(20);
+myMonster.attack1();
+myMonster.attack2();
+console.log(myMonster.power);
+
+myMonster.power = 10;
