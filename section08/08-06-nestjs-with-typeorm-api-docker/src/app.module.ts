355932208b30c10216ac75432cc5d5b87a15b25f
larmong <larmong@naver.com>
2024-05-15 01:19:33 +0900

    08-06-nestjs-with-typeorm-api-docker

--- section08/08-06-nestjs-with-typeorm-api-docker/src/app.module.ts
+++ section08/08-06-nestjs-with-typeorm-api-docker/src/app.module.ts
@@ -2,6 +2,7 @@ import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { GraphQLModule } from "@nestjs/graphql";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 
 import { BoardsModule } from "./apis/boards/boards.module";
 import { Board } from "./apis/boards/entities/board.entity";
@@ -12,6 +13,8 @@ import { Board } from "./apis/boards/entities/board.entity";
     // ProductsModule,
     // UsersModule,
 
+    ConfigModule.forRoot(), // process.env 보다 위에 설정되어야 함!
+
     // GraphQL 설정
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
@@ -20,12 +23,12 @@ import { Board } from "./apis/boards/entities/board.entity";
 
     // typeorm
     TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "127.0.0.1",
-      port: 3306,
-      username: "root",
-      password: "",
-      database: "project",
+      type: process.env.DATABASE_TYPE as "mysql",
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
       entities: [Board],
       synchronize: true,
       logging: true,
